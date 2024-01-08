f38f3feea4b804ef0112410007f4243a397784a7
Imahn Shekhzadeh <imahn.shekhzadeh@hesge.ch>
2024-01-08 03:36:16 +0100

    remove obsolete shell script in `transformers` dir

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip install pytorch-lightning
-pip install py-cpuinfo
-pip install seaborn
