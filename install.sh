ad47753556ece5b5f521e0512565062f0bbbe077
Imahn Shekhzadeh <imahn.shekhzadeh@hesge.ch>
2024-01-08 03:32:43 +0100

    refactor: remove obsolete directory

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install pytorch-lightning
+pip install py-cpuinfo
+pip install seaborn
